b35364062cb6841d1eb1ee04b9e71e67fe71255f
oguz <oguz.oeztuerk@outlook.de>
2025-06-21 22:45:37 +0200

    Update Home page layout and standardize CSS variables
    - Refined Home page layout with centered content and updated styles.
    - Standardized CSS variable naming conventions for better consistency.
    - Integrated `tailwindcss-animate` plugin for animation support.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl">StoreIt</h1>
+    <div className="flex h-screen items-center justify-center bg-amber-100">
+      <h1 className="text-3xl font-bold text-brand-100">StoreIt</h1>
     </div>
   );
 }
