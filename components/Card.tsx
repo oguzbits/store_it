c1b83e5d2c5fa52afd4050714acf92db33095b70
oguz <oguz.oeztuerk@outlook.de>
2025-06-23 23:48:15 +0200

    Add `FileDetails` component and enhance `ActionDropdown` with file details support
    - Introduced `FileDetails` component to display detailed file information, including format, size, owner, and last edited date.
    - Enhanced `ActionDropdown` to include a new "details" option utilizing `FileDetails` for detailed view.
    - Updated `globals.css` to remove unused Tailwind configuration import.
    - Added new dependencies: `clsx`, `lucide-react`, and updated existing `react-hook-form`.
    - Adjusted `package.json` overrides for `react` and `react-dom` versions.

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Models } from "node-appwrite";
 import Link from "next/link";
 import Thumbnail from "@/components/Thumbnail";
@@ -22,20 +21,19 @@ const Card = ({ file }: { file: Models.Document }) => {
           <ActionDropdown file={file} />
           <p className="body-1">{convertFileSize(file.size)}</p>
         </div>
+      </div>
 
-        <div className="file-card-details">
-          <p className="subtitle-2 line-clamp-1">{file.name}</p>
-          <FormattedDateTime
-            date={file.$createdAt}
-            className="body-2 text-light-100"
-          />
-          <p className="caption line-clamp-1 text-light-100">
-            By: {file.owner.fullName}
-          </p>
-        </div>
+      <div className="file-card-details">
+        <p className="subtitle-2 line-clamp-1">{file.name}</p>
+        <FormattedDateTime
+          date={file.$createdAt}
+          className="body-2 text-light-100"
+        />
+        <p className="caption line-clamp-1 text-light-200">
+          By: {file.owner.fullName}
+        </p>
       </div>
     </Link>
   );
 };
-
 export default Card;
